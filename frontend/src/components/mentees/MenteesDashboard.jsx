b4f843e2e3874f2d8912d1901c4880e01748cd86
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-07-25 14:26:16 +0300

    chore: remove commented-out code in MenteesDashboard component

--- frontend/src/components/mentees/MenteesDashboard.jsx
+++ frontend/src/components/mentees/MenteesDashboard.jsx
@@ -9,13 +9,6 @@ import "jspdf-autotable"; // For table support
 import { useEffect, } from "react";
 import axiosInstance from "../../utils/axiosInstance";
 
-
-// const MenteesDashboard = () => {
-//   const [mentees, setMentees] = useState([
-//     { id: 1, name: "Absolom Jr", email: "absolom@gmail.com", year: "2025", status: "Active" },
-//     { id: 2, name: "Isaac Nabasa", email: "isaac@gmail.com", year: "2024", status: "Pending" },
-//   ]);
-
 const MenteesDashboard = () => {
   const [mentees, setMentees] = useState([]);
   const [loading, setLoading] = useState(true);
