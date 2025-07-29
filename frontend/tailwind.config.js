a5098bd2350200f77596bb0a5be4fa3b3a68a41c
Absolomjr <145746728+Absolomjr@users.noreply.github.com>
2025-07-29 13:42:34 +0300

    feat: implement theme toggle functionality in the application

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // Enables manual dark mode via `class`
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
